5836121663f1a3b506e6d0f5010e17c6b117b6d2
SeungJu0406 <nss8164@gmail.com>
2024-08-12 14:57:41 +0900

    [feat] 전체적 클래스 틀 작성
    음식, 맵, 몬스터, 플레이어, 장면, 게임 클래스

---
+++ TextEternalReturn/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TextEternalReturn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ TextEternalReturn/Scene/Scene/BonfireScene.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEternalReturn.Scene.Scene
+{
+    internal class BonfireScene
+    {
+    }
+}

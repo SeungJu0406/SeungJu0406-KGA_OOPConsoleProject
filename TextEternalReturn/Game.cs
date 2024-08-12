e6e390640c181686755799df27b79c035af69557
SeungJu0406 <nss8164@gmail.com>
2024-08-12 15:00:56 +0900

    [feat] 게임 루프 작성

--- TextEternalReturn/Game.cs
+++ TextEternalReturn/Game.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TextEternalReturn
 {
-    internal class Game
+    public class Game
     {
+        bool isRunning;
+        public void Run()
+        {
+            Start();
+            while (isRunning)
+            {
+                Render();
+                Input();
+                Update();
+            }
+            End();
+        }
+
+        private void Start()
+        {
+
+        }
+        private void Render()
+        {
+
+        }
+        private void Input()
+        {
+
+        }
+        private void Update()
+        {
+
+        }
+        private void End()
+        {
+
+        }
     }
 }

891742d64f8eecdf0b2bc098563e15e4791064df
SeungJu0406 <nss8164@gmail.com>
2024-08-12 15:32:43 +0900

    [refactor] 열거형 이름 변경
    클래스명이랑 겹쳐서 이름 바꿈

--- TextEternalReturn/Enums.cs
+++ TextEternalReturn/Enums.cs
@@ -1,23 +1,21 @@
 ﻿namespace TextEternalReturn
 {
-
-        public enum Scene
-        {
-            StartScene, EndScene,
-            MapScene, HotelScene, PoliceOfficeScene, HarborScene, HospitalScene, BonFireScene,
-            StatusScene, InventoryScene, BattleScene, SIZE
-        }
-        public enum Place
-        {
-            Hotel, PoliceOffice, Harbor, Hospital, BonFire, SIZE
-        }
-        public enum Monster
-        {
-            Hyunwoo, Chicken, WildBoar, Wolf, Bear,SIZE
-        }
-        public enum Food
-        {
-            SalmonSteak, FishCuttlet, Steak, Salmon, CodFish, Meat
-        }
-
+    public enum SceneType
+    {
+        StartScene, EndScene,
+        MapScene, HotelScene, PoliceOfficeScene, HarborScene, HospitalScene, BonFireScene,
+        StatusScene, InventoryScene, BattleScene, SIZE
+    }
+    public enum PlaceType
+    {
+        Hotel, PoliceOffice, Harbor, Hospital, BonFire, SIZE
+    }
+    public enum MonsterType
+    {
+        Hyunwoo, Chicken, WildBoar, Wolf, Bear, SIZE
+    }
+    public enum FoodType
+    {
+        SalmonSteak, FishCuttlet, Steak, Salmon, CodFish, Meat
+    }
 }

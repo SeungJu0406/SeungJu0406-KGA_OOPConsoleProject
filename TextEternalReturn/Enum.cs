e12e451ca7da749c855d72f6432b066cb7d80ca9
SeungJu0406 <nss8164@gmail.com>
2024-08-12 15:27:47 +0900

    [feat] 필요 열거형 작성

---
+++ TextEternalReturn/Enum.cs
@@ -0,0 +1,24 @@
+﻿namespace TextEternalReturn
+{
+    public static class Enum
+    {
+        public enum Scene
+        {
+            StartScene, EndScene,
+            MapScene, HotelScene, PoliceOfficeScene, HarborScene, HospitalScene, BonFireScene,
+            StatusScene, InventoryScene, BattleScene, SIZE
+        }
+        public enum Place
+        {
+            Hotel, PoliceOffice, Harbor, Hospital, BonFire, SIZE
+        }
+        public enum Monster
+        {
+            Hyunwoo, Chicken, WildBoar, Wolf, Bear,SIZE
+        }
+        public enum Food
+        {
+            SalmonSteak, FishCuttlet, Steak, Salmon, CodFish, Meat
+        }
+    }
+}

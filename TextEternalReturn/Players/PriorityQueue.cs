70f39218c78e730fb78b78c4052fd56428a516fc
SeungJu0406 <nss8164@gmail.com>
2024-08-13 01:14:21 +0900

    [feat] 우선순위 큐 Remove 기능 추가

--- TextEternalReturn/Players/PriorityQueue.cs
+++ TextEternalReturn/Players/PriorityQueue.cs
@@ -83,6 +83,35 @@ namespace TextEternalReturn.Players
             }
             return result.food;
         }
+        public void Remove(Food food)
+        {
+            if (Count <= 0)
+                throw new IndexOutOfRangeException();
+            MinQueue findfood = new MinQueue() { food = food , priority = food.id};
+            int index = Array.IndexOf(queue, findfood);
+            queue[index] = queue[Count - 1]; // 마지막과 교체
+            queue[Count - 1] = default;
+            Count--;
+            int now = 0; // 현재 인덱스
+            while (now <= Count) //더 이상 내려갈 수 없을 때
+            {
+                int next = now; // next: 교체 할 자식노드 인덱스
+                int left = next * 2 + 1;
+                int right = next * 2 + 2;
+                if (left <= Count - 1 && queue[now].priority >= queue[left].priority ||
+                    right <= Count - 1 && queue[now].priority >= queue[right].priority) // 좌우측 둘중하나보다 클 때
+                {
+                    next = queue[left].priority < queue[right].priority ? left : right;
+                }
+                else
+                    break;
+                MinQueue temp = queue[now];
+                queue[now] = queue[next];
+                queue[next] = temp;
+                //현재 위치 갱신
+                now = next;
+            }
+        }
         public Food Peek()
         {
             return queue[0].food;

43cb7df312ab98c443f6ed505b6c517fe4d05f66
SeungJu0406 <nss8164@gmail.com>
2024-08-13 23:42:50 +0900

    [feat] 맵 기반 틀 구현
    아직 기능은없음 출력만 구현

--- TextEternalReturn/Scenes/Scenes/MapScene.cs
+++ TextEternalReturn/Scenes/Scenes/MapScene.cs
@@ -1,21 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TextEternalReturn.Players;
+﻿using TextEternalReturn.Players;
 
 namespace TextEternalReturn.Scenes.Scenes
 {
     internal class MapScene : Scene
     {
+        enum MapPoint
+        {
+            Map, X0, X1, X2, X3, X4, X5, X6,
+            Y0,
+            Y1,
+            Y2,
+            Y3,
+            Y4,
+            Y5,
+            Y6,
+            Player, SIZE
+        }
+        Point[] points = new Point[(int)MapPoint.SIZE];
         public MapScene(Player player) : base(player)
         {
-
+            points[(int)MapPoint.Map] = new Point() { x = X, y = Y };
+            points[(int)MapPoint.Player] = new Point() { x = X + 4, y = Y + 4 };
         }
         public override void Render()
         {
-            throw new NotImplementedException();
+            PrintMap();
         }
         public override void Input()
         {
@@ -23,15 +32,54 @@ namespace TextEternalReturn.Scenes.Scenes
         }
         public override void Update()
         {
-            
+
         }
         public override void Enter()
         {
-
+            Console.Clear();
         }
         public override void Exit()
         {
             game.prevScene = this;
+        }
+        private void PrintMap()
+        {
+            Point map = points[(int)MapPoint.Map];
+            SetCursor(map);
+            map.y++;
+            Console.WriteLine("#########");
+            SetCursor(map);
+            map.y++;
+            Console.WriteLine("#       #");
+            SetCursor(map);
+            map.y++;
+            Console.WriteLine("#       #");
+            SetCursor(map);
+            map.y++;
+            Console.WriteLine("#       #");
+            SetCursor(map);
+            map.y++;
+            Console.WriteLine("#       #");
+            SetCursor(map);
+            map.y++;
+            Console.WriteLine("#       #");
+            SetCursor(map);
+            map.y++;
+            Console.WriteLine("#       #");
+            SetCursor(map);
+            map.y++;
+            Console.WriteLine("#       #");
+            SetCursor(map);
+            map.y++;
+            Console.WriteLine("#########");
+            SetCursor(points[(int)MapPoint.Player]);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("P");
+            Console.ResetColor();
+        }
+        private void SetCursor(Point cursorPoint)
+        {
+            Console.SetCursorPosition(cursorPoint.x, cursorPoint.y);
         }
     }
 }

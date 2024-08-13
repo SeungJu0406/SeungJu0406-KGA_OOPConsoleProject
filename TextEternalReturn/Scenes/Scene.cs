94379c31d712c6137510430cbe4dcf721fef8796
SeungJu0406 <nss8164@gmail.com>
2024-08-13 14:58:16 +0900

    [refactor] 스테이터스 출력 순서 변경
    몬스터와 체력 공격력이 같은줄에서 출력하게끔 바꿈, 나중에 경험치를 레벨로 바꿀거임

--- TextEternalReturn/Scenes/Scene.cs
+++ TextEternalReturn/Scenes/Scene.cs
@@ -4,16 +4,18 @@ namespace TextEternalReturn.Scenes
 {
     public abstract class Scene
     {
-        protected Game game;
-        protected Scene curScene;
-        protected ConsoleKey consoleKey;
-        protected Player player;
-        protected Point statusPoint;
         public struct Point()
         {
             public int x, y;
         }
+        protected Game game;
+        protected Player player;
+        protected ConsoleKey consoleKey;
+
+        protected Scene curScene;
         protected Point curPoint;
+
+        protected Point statusPoint;
         protected Scene(Player player)
         {
             this.game = Game.getInstance();
@@ -53,11 +55,13 @@ namespace TextEternalReturn.Scenes
         protected void PrintStatus()
         {
             SetCursor(statusPoint);
-            Console.WriteLine($"체력: {player.curHp,5}/{player.maxHp}");
+            Console.WriteLine($"경험치: {player.exp,+5}");        
             statusPoint.y++;
+            SetCursor(statusPoint);
             Console.WriteLine($"공격력: {player.power,5}");
             statusPoint.y++;
-            Console.WriteLine($"경험치: {player.exp,+5}");
+            SetCursor(statusPoint);
+            Console.WriteLine($"체력: {player.curHp,5}/{player.maxHp}");
             statusPoint.y = 0;
         }
         private void SetCursor(Point cursorPoint)

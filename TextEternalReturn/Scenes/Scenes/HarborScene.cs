326eb7f7ebbdfe8cc1b3d26882756340d8c9e530
SeungJu0406 <nss8164@gmail.com>
2024-08-14 23:46:46 +0900

    [feat] 호텔, 경찰서, 병원 항구 기능 구현

--- TextEternalReturn/Scenes/Scenes/HarborScene.cs
+++ TextEternalReturn/Scenes/Scenes/HarborScene.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,22 @@ namespace TextEternalReturn.Scenes.Scenes
 {
     internal class HarborScene : Scene
     {
+        enum Pos { Place ,Chest, Rest, Fishing ,MoveMap, Inventory, SIZE }
+        Point[] points = new Point[(int)Pos.SIZE];
         public HarborScene(Player player) : base(player)
         {
-
+            points[(int)Pos.Place] = new Point() { x = X, y = Y + 0 };
+            points[(int)Pos.Chest] = new Point() { x = X, y = Y + 2 };
+            points[(int)Pos.Rest] = new Point() { x = X, y = Y + 3 };
+            points[(int)Pos.Fishing] = new Point() { x = X, y = Y + 4 };
+            points[(int)Pos.MoveMap] = new Point() { x = X, y = Y + 5 };
+            points[(int)Pos.Inventory] = new Point() { x = X, y = Y + 6 };
+            curPoint = points[(int)Pos.Chest];
         }
         public override void Render()
         {
             PrintStatus();
+            PrintHarbor();
         }
         public override void Update()
         {
@@ -29,22 +39,60 @@ namespace TextEternalReturn.Scenes.Scenes
         {
             game.prevScene = this;
         }
-        private void PrintHotel()
+        private void PrintHarbor()
         {
-
+            SetCursor(points[(int)Pos.Place]);
+            Console.WriteLine(" 항 구 ");
+            SetCursor(points[(int)Pos.Chest]);
+            Console.WriteLine("▷ 상자 열기");
+            SetCursor(points[(int)Pos.Rest]);
+            Console.WriteLine("▷ 휴식");
+            SetCursor(points[(int)Pos.Fishing]);
+            Console.WriteLine("▷ 낚시 하기");
+            SetCursor(points[(int)Pos.MoveMap]);
+            Console.WriteLine("▷ 이동 하기");
+            SetCursor(points[(int)Pos.Inventory]);
+            Console.WriteLine("▷ 아이템 확인");
+            SetCursor(curPoint);
+            Console.WriteLine("▶");
         }
         protected override void PushKeyZ()
         {
-            base.PushKeyZ();
+            if (curPoint.y == points[(int)Pos.Chest].y)
+            {
+                game.ChangeScene(SceneType.ChestScene);
+            }
+            else if (curPoint.y == points[(int)Pos.Rest].y)
+            {
+                player.Rest();
+            }
+            else if (curPoint.y == points[(int)Pos.Fishing].y)
+            {
+                game.ChangeScene(SceneType.FishingScene);
+            }
+            else if (curPoint.y == points[(int)Pos.MoveMap].y)
+            {
+                game.ChangeScene(SceneType.MapScene);
+            }
+            else if (curPoint.y == points[(int)Pos.Inventory].y)
+            {
+                game.ChangeScene(SceneType.InventoryScene);
+            }
         }
         #region 커서 이동
         protected override void MoveUpCursor()
         {
-            base.MoveUpCursor();
+            if (curPoint.y > points[(int)Pos.Chest].y)
+            {
+                curPoint.y -= 1;
+            }
         }
         protected override void MoveDownCursor()
         {
-            base.MoveDownCursor();
+            if (curPoint.y < points[(int)Pos.Inventory].y)
+            {
+                curPoint.y += 1;
+            }
         }
         protected override void MoveLeftCursor()
         {
--- TextEternalReturn/Scenes/Scenes/PoliceOfficeScene.cs
+++ TextEternalReturn/Scenes/Scenes/PoliceOfficeScene.cs
@@ -1,21 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TextEternalReturn.Players;
+﻿using TextEternalReturn.Players;
 
 namespace TextEternalReturn.Scenes.Scenes
 {
-    internal class PoliceOfficeScene :Scene
+    internal class PoliceOfficeScene : Scene
     {
+        enum Pos { Place,Chest, Rest, MoveMap, Inventory, SIZE }
+        Point[] points = new Point[(int)Pos.SIZE];
         public PoliceOfficeScene(Player player) : base(player)
         {
-
+            points[(int)Pos.Place] = new Point() { x = X, y = Y + 0 };
+            points[(int)Pos.Chest] = new Point() { x = X, y = Y + 2 };
+            points[(int)Pos.Rest] = new Point() { x = X, y = Y + 3 };
+            points[(int)Pos.MoveMap] = new Point() { x = X, y = Y + 4 };
+            points[(int)Pos.Inventory] = new Point() { x = X, y = Y + 5 };
+            curPoint = points[(int)Pos.Chest];
         }
         public override void Render()
         {
             PrintStatus();
+            PrintPoliceOffice();
         }
         public override void Update()
         {
@@ -29,22 +32,54 @@ namespace TextEternalReturn.Scenes.Scenes
         {
             game.prevScene = this;
         }
-        private void PrintHotel()
+        private void PrintPoliceOffice()
         {
-
+            SetCursor(points[(int)Pos.Place]);
+            Console.WriteLine(" 경 찰 서 ");
+            SetCursor(points[(int)Pos.Chest]);
+            Console.WriteLine("▷ 상자 열기");
+            SetCursor(points[(int)Pos.Rest]);
+            Console.WriteLine("▷ 휴식");
+            SetCursor(points[(int)Pos.MoveMap]);
+            Console.WriteLine("▷ 이동 하기");
+            SetCursor(points[(int)Pos.Inventory]);
+            Console.WriteLine("▷ 아이템 확인");
+            SetCursor(curPoint);
+            Console.WriteLine("▶");
         }
         protected override void PushKeyZ()
         {
-            base.PushKeyZ();
+            if (curPoint.y == points[(int)Pos.Chest].y)
+            {
+                game.ChangeScene(SceneType.ChestScene);
+            }
+            else if (curPoint.y == points[(int)Pos.Rest].y)
+            {
+                player.Rest();
+            }
+            else if (curPoint.y == points[(int)Pos.MoveMap].y)
+            {
+                game.ChangeScene(SceneType.MapScene);
+            }
+            else if (curPoint.y == points[(int)Pos.Inventory].y)
+            {
+                game.ChangeScene(SceneType.InventoryScene);
+            }
         }
         #region 커서 이동
         protected override void MoveUpCursor()
         {
-            base.MoveUpCursor();
+            if (curPoint.y > points[(int)Pos.Chest].y)
+            {
+                curPoint.y -= 1;
+            }
         }
         protected override void MoveDownCursor()
         {
-            base.MoveDownCursor();
+            if (curPoint.y < points[(int)Pos.Inventory].y)
+            {
+                curPoint.y += 1;
+            }
         }
         protected override void MoveLeftCursor()
         {

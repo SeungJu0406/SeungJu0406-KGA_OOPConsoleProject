9f8eea968b19e0a133e6c3582bd2a5f8f07b4aca
SeungJu0406 <nss8164@gmail.com>
2024-08-14 23:47:08 +0900

    [feat] 낚시 장면 구현
    항구에서 선택가능한 장면

---
+++ TextEternalReturn/Scenes/Scenes/FishingScene.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextEternalReturn.Players;
+
+namespace TextEternalReturn.Scenes.Scenes
+{
+    public class FishingScene : Scene
+    {
+        public FishingScene(Player player) : base(player) 
+        {
+
+        }
+        public override void Enter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Exit()
+        {
+            game.prevScene = this;
+        }
+
+        public override void Render()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+        #region 커서 이동
+        protected override void MoveUpCursor()
+        {
+            base.MoveUpCursor();
+        }
+        protected override void MoveDownCursor()
+        {
+            base.MoveDownCursor();
+        }
+
+        protected override void MoveLeftCursor()
+        {
+            base.MoveLeftCursor();
+        }
+
+        protected override void MoveRightCursor()
+        {
+            base.MoveRightCursor();
+        }
+        #endregion
+        protected override void PushKeyZ()
+        {
+            base.PushKeyZ();
+        }
+    }
+}
